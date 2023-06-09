b0156bd3c9b80cd97c31abf6f87af6af20d57210
AP <aleksandrpolishchuk@gmail.com>
2023-06-09 23:58:22 +0700

    add VideoEntity

---
+++ backend/backend/Entities/VideoEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace backend.Entities
+{
+	public class VideoEntity
+	{
+		public long Id { get; set; }
+		public string Url { get; set; }
+		public string Title { get; set; }
+		public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+	}
+}
