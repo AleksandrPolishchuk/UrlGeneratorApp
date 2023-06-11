2a39ee06f1e76e08a528889742fa2c4b14e06cee
AP <aleksandrpolishchuk@gmail.com>
2023-06-11 23:55:14 +0700

    add ApplicationDbContext (backend)

--- backend/backend/Contex/ApplicationDbContext.cs
+++ backend/backend/Contex/ApplicationDbContext.cs
@@ -1,6 +1,14 @@
-﻿namespace backend.Contex
+﻿using backend.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend.Contex
 {
-	public class ApplicationDbContext
+	public class ApplicationDbContext : DbContext
 	{
+		public ApplicationDbContext(DbContextOptions options) : base(options)
+		{
+		}
+
+		public DbSet<VideoEntity> Videos { get; set; }  
 	}
 }
