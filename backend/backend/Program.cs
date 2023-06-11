088c4ef17503a1ddfca1267a86b45b140422d253
AP <aleksandrpolishchuk@gmail.com>
2023-06-11 23:59:36 +0700

    add Db config (backend)

--- backend/backend/Program.cs
+++ backend/backend/Program.cs
@@ -1,6 +1,15 @@
+using backend.Contex;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+// DB Configuration
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+	options.UseSqlServer("");
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
