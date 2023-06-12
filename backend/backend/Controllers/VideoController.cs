e8474b4e2ae75b90c8e14b016157df64403ba4de
AP <aleksandrpolishchuk@gmail.com>
2023-06-12 23:29:57 +0700

    add VideoController (backend)

---
+++ backend/backend/Controllers/VideoController.cs
@@ -0,0 +1,26 @@
+﻿using backend.Contex;
+using backend.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class VideoController : ControllerBase
+	{
+		// Access to DB
+		private readonly ApplicationDbContext _context;
+
+		public VideoController(ApplicationDbContext context)
+		{
+			_context = context;
+		}
+
+
+		// CRUD
+
+		[HttpPost]
+		public async Task<ActionResult<VideoEntity>> CreateNewVideo()
+	}
+}
