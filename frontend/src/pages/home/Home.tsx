6e842bc17db43ed2ddb9060d4d77db5879846bb3
AP <aleksandrpolishchuk@gmail.com>
2023-06-15 22:05:12 +0700

    add useEffect for Home page (frontend)

--- frontend/src/pages/home/Home.tsx
+++ frontend/src/pages/home/Home.tsx
@@ -1,9 +1,20 @@
 import "./home.scss";
 import { AddCircle, Edit, Delete } from "@mui/icons-material";
 import { useState, useEffect } from "react";
+import { IVideo } from "../../types/global.typing";
+import { baseUrl } from "../../constants/url.constant";
+import axios from "axios";
 
 const Home = () => {
-  const [videos, setVideos] = useState([]);
+  const [videos, setVideos] = useState<IVideo[]>([]);
+
+  useEffect(() => {
+    axios
+      .get<IVideo[]>(baseUrl)
+      .then((response) => setVideos(response.data))
+      .catch((error) => alert(JSON.stringify(error)));
+  }, []);
+
   return (
     <div className="home">
       <div className="heading">
