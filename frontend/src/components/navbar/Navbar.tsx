702c9aadc8c21ccb8ebca962173db5ddc6e993b5
AP <aleksandrpolishchuk@gmail.com>
2023-06-15 21:17:36 +0700

    add Navigation to Navbar (frontend)

--- frontend/src/components/navbar/Navbar.tsx
+++ frontend/src/components/navbar/Navbar.tsx
@@ -1,9 +1,11 @@
 import "./navbar.scss";
+import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
+  const redirect = useNavigate();
   return (
     <div className="navbar">
-      <span>Url Generator</span>
+      <span onClick={() => redirect("/")}>Url Generator</span>
     </div>
   );
 };
