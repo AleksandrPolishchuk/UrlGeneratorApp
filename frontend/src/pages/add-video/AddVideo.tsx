92c0f39d2fc433b20b4665f486f4fe862f0f0712
AP <aleksandrpolishchuk@gmail.com>
2023-06-16 23:59:29 +0700

    add btns for AddVideo page (frontend)

--- frontend/src/pages/add-video/AddVideo.tsx
+++ frontend/src/pages/add-video/AddVideo.tsx
@@ -1,9 +1,15 @@
+import { Button, TextField } from "@mui/material";
 import "./add-video.scss";
 
 const AddVideo = () => {
   return (
     <div className="add-video">
       <h1>Add Video</h1>
+      <div className="inputs">
+        <TextField autoComplete="off" label="Video Title" variant="outlined" />
+        <Button>Save</Button>
+        <Button>Back To List</Button>
+      </div>
     </div>
   );
 };
