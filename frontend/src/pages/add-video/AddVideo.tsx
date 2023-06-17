4cbdfbb70803f3af4e73fb8f9dcbf93da3646e9e
AP <aleksandrpolishchuk@gmail.com>
2023-06-17 23:27:08 +0700

    add jsx for AddVideo page (frontend)

--- frontend/src/pages/add-video/AddVideo.tsx
+++ frontend/src/pages/add-video/AddVideo.tsx
@@ -1,14 +1,53 @@
+import { useState } from "react";
 import { Button, TextField } from "@mui/material";
 import "./add-video.scss";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { baseUrl } from "../../constants/url.constant";
 
 const AddVideo = () => {
+  const [title, setTitle] = useState<string>("");
+  const redirect = useNavigate();
+
+  const handleClickSaveBtn = () => {
+    if (title === "") {
+      alert("Please Enter Something");
+      return;
+    }
+    const data = {
+      title,
+    };
+
+    axios
+      .post(baseUrl, data)
+      .then((response) => redirect("/"))
+      .catch((error) => alert("Error Ocuured"));
+  };
+
+  const handleClickBackBtn = () => {
+    redirect("/");
+  };
   return (
     <div className="add-video">
       <h1>Add Video</h1>
       <div className="inputs">
-        <TextField autoComplete="off" label="Video Title" variant="outlined" />
-        <Button variant="outlined">Save</Button>
-        <Button variant="outlined">Back To List</Button>
+        <TextField
+          autoComplete="off"
+          label="Video Title"
+          variant="outlined"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <Button variant="outlined" onClick={handleClickSaveBtn}>
+          Save
+        </Button>
+        <Button
+          variant="outlined"
+          color="secondary"
+          onClick={handleClickBackBtn}
+        >
+          Back To List
+        </Button>
       </div>
     </div>
   );
